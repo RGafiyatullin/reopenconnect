0f7409f9f9e4d6e4deb31867fe96495239f8cb7b
Roman Gafiyatullin <r.gafiyatullin@me.com>
2021-09-14 21:27:36 +0300

    Do not setup routes. Let the on-connected script do it

--- src/cli/reopenconnect/context/tun_dev_context.rs
+++ src/cli/reopenconnect/context/tun_dev_context.rs
@@ -5,20 +5,20 @@ use super::*;
 #[async_trait::async_trait]
 impl TunDevContext for Context {
     async fn tun_dev_created(&mut self, tun_dev_name: &str) -> Result<(), AnyError> {
-        for network in &self.split_routes_ipv4 {
-            let mut command = ::tokio::process::Command::new("/sbin/ip");
-            command
-                .arg("route")
-                .arg("add")
-                .arg(network.to_string())
-                .arg("dev")
-                .arg(tun_dev_name)
-                .arg("proto")
-                .arg("unspec")
-                .arg("scope")
-                .arg("link");
-            let _ = command.status().await?;
-        }
+        // for network in &self.split_routes_ipv4 {
+        //     let mut command = ::tokio::process::Command::new("/sbin/ip");
+        //     command
+        //         .arg("route")
+        //         .arg("add")
+        //         .arg(network.to_string())
+        //         .arg("dev")
+        //         .arg(tun_dev_name)
+        //         .arg("proto")
+        //         .arg("unspec")
+        //         .arg("scope")
+        //         .arg("link");
+        //     let _ = command.status().await?;
+        // }
 
         Ok(())
     }
